9175c019b8e1b5f0196061833cb4afd894f7b5e5
rashmimishra-0387 <64351770+rashmimishra-0387@users.noreply.github.com>
2020-08-04 09:27:12 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,21 @@
+class Chain {
+    constuctor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+            console.log(bodyA.position.x);
+          this.chain = Constraint.create(options);
+          World.add(world, this.chain);
+    }
+
+    display(){
+       var pointA = this.chain.bodyA.position;
+       var PointB = this.chain.bodyB.position;
+        strokeWeight(5);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+    }
+}
